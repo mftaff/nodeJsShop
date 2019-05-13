9abaa643e959ce5515c1d0053cfea656d8a7b730
Meir Taffel <artgardening@gmail.com>
2019-05-13 17:16:13 +0300

    Complete through section 9 (before sql)

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,23 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-exports.getProducts =  (req, res, next) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/product-list', {
       pageTitle: 'All Products',
       path: '/products',
       prods: products
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      pageTitle: product.title,
+      path: '/products',
+      product: product
     });
   });
 };
@@ -21,10 +33,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: '/cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+        products: cartProducts
+      })
+    })
   })
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
@@ -32,6 +64,14 @@ exports.getOrders = (req, res, next) => {
     pageTitle: 'Your Orders',
     path: '/orders'
   })
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
