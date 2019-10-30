64752b7551f7a81f84b4c96900a35cbcc8bce5f4
Meir Taffel <artgardening@gmail.com>
2019-10-30 19:28:54 -0400

    Replace session user with mongodb user in controllers

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,7 +42,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.session.user
+  req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
@@ -61,7 +61,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     }).then(result => {
       console.log(result);
       res.redirect('/cart');
@@ -72,7 +72,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.session.user
+  req.user
     .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
@@ -81,7 +81,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  req.session.user
+  req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
@@ -90,15 +90,15 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          userId: req.session.user,
-          name: req.session.user.name
+          userId: req.user,
+          name: req.user.name
         },
         products: products
       });
       return order.save();
     })
     .then(result => {
-      return req.session.user.clearCart();
+      return req.user.clearCart();
     })
     .then(() => {
       res.redirect('/orders');
@@ -107,8 +107,7 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  console.log(req.session.user);
-  Order.find({'user.userId': req.session.user._id})
+  Order.find({'user.userId': req.user._id})
     .then(orders => {
       res.render('shop/orders', {
         pageTitle: 'Your Orders',
