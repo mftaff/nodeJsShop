709dfad5cac39ef2c14d622a79aad6e198cd6925
Meir Taffel <artgardening@gmail.com>
2019-10-30 19:09:39 -0400

    Add session logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,3 +16,10 @@ exports.postLogin = (req, res, next) => {
       res.redirect('/');
     })
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
