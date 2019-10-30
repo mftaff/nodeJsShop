3904bbfbffd0db62da94258797fabdced5dfc401
Meir Taffel <artgardening@gmail.com>
2019-10-30 18:43:33 -0400

    Add mongodb session storage

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://meir:qQY2ByJrwvU4hLLm@cluster0-zc7pz.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +26,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('5daf0de6c16ea33330ba2550')
@@ -36,7 +52,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://meir:qQY2ByJrwvU4hLLm@cluster0-zc7pz.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
   ).then(result => {
     User.findOne().then(user => {
       if (!user) {
